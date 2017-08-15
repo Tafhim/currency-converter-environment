e99addbe99f35b484b7f320e6bf3fd15408ff726
Tafhim <tafhim.ul.islam@gmail.com>
2017-08-15 14:15:49 +0600

    Add first boot script

--- first_boot.sh
+++ first_boot.sh
@@ -10,3 +10,7 @@ docker-compose down --remove-orphans && docker-compose build && docker-compose u
 
 # Pull using composer
 docker exec -ti -u tafhim zend-cconvert-php bash -c "cd /var/www/html/public && composer install"
+
+# Migrate db imports
+docker exec -ti -u tafhim zend-cconvert-php bash -c "cd /var/www/html/public && php /var/www/html/public/vendor/robmorgan/phinx/bin/phinx migrate"
+docker exec -ti -u tafhim zend-cconvert-php bash -c "cd /var/www/html/public && php /var/www/html/public/vendor/robmorgan/phinx/bin/phinx seed:run"
